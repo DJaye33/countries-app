b3ce4a49f5dd64180dccd8f33214bd9cd589345f
Danny JeanLouis <dlouis3386@gmail.com>
2022-04-10 03:37:22 -0400

    add dark theme to app background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React, { useContext } from "react";
 import Header from "./components/Header/Header";
+import { ThemeContext } from "./context/ThemeContext";
 
 import "./App.scss";
 
 function App() {
+  const context = useContext(ThemeContext);
+
   return (
-    <div className="App">
+    <div className={`App App--${context.theme === "dark" ? "dark" : ""}`}>
       <Header />
     </div>
   );
